cba43f94703f5a241cf8f17b2e0dd9c991cc459b
Project-MAR <atitep.ram@gmail.com>
2017-06-11 01:50:22 +0700

    button and switch is completed

--- KATIP-V2.0/KATIP-V2.0.ino
+++ KATIP-V2.0/KATIP-V2.0.ino
@@ -42,52 +42,90 @@
 #include <BlynkSimpleEsp8266.h>
 #include "token.h"
 
-int AppButtonStatus = 0;
+/* ---------- IO Define ----------------*/
+const int LED1_PIN = 5;
+const int LED2_PIN = 4;
+const int LED3_PIN = 0;
+
+const int RelayA_PIN = 2;
+const int RelayC_PIN = 14;
+
+const int Switch_PIN = 10;
+/*---------------------------------------*/
+
+int LightStatus = 0;
+
+int lastSwitchState    = 0;
+int currentSwitchState = 0;
+
+void toggleLight()
+{
+  if(LightStatus == 0)
+  {
+    Blynk.virtualWrite(V0, LOW);
+    Serial.println("SET Light => OFF");
+  }
+  else
+  {
+    Blynk.virtualWrite(V0, HIGH);
+    Serial.println("SET Light => ON");
+  }
+  Serial.println("");
+}
 
 // Button event (Attach to V0) from Blynk App
 BLYNK_WRITE(V0)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V0 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
-  Serial.print("V0 Buttun value is: ");
-  Serial.println(pinValue);
-  AppButtonStatus = pinValue;
+  //int pinValue = param.asInt();
+  Serial.print("[APP Event] : ");
+  
+  LightStatus ^= 1;
+  toggleLight();
 }
 
 BlynkTimer timer;
 void myTimerEvent()
 {
-    if(AppButtonStatus == 1)
+    currentSwitchState = digitalRead(Switch_PIN);
+    if (currentSwitchState != lastSwitchState)
     {
-      AppButtonStatus = 0;
-      Blynk.virtualWrite(V0, LOW);
-      Serial.println("BlunkTimer: SET to OFF");
-    }
-    else
-    {
-      AppButtonStatus = 1;
-      Blynk.virtualWrite(V0, HIGH);
-      Serial.println("BlunkTimer: SET to ON");
+      Serial.print("[Manual Event] : ");
+      Serial.print(lastSwitchState);
+      Serial.print(" to ");
+      Serial.println(currentSwitchState);
+      
+      lastSwitchState = currentSwitchState;
+
+      LightStatus ^= 1;
+      toggleLight();
     }
 }
 
 void setup()
 {
+  pinMode(RelayA_PIN, OUTPUT);
+  pinMode(RelayC_PIN, OUTPUT);
+  digitalWrite(RelayA_PIN, HIGH);
+  digitalWrite(RelayC_PIN, LOW);
+  
   // Debug console
   Serial.begin(115200);
-
   Blynk.begin(auth, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+  timer.setInterval(100L, myTimerEvent);
 
-  timer.setInterval(5000L, myTimerEvent);
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(LED3_PIN, OUTPUT);
+  digitalWrite(LED1_PIN, HIGH);
+  digitalWrite(LED2_PIN, HIGH);
+  digitalWrite(LED3_PIN, HIGH);
+
+  pinMode(Switch_PIN, INPUT);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run(); 
 }
 
